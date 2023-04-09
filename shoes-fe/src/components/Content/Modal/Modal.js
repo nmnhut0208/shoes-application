099e4b288f54c22d10ad6cca79aa96afe62afe09
Kieu Thu <kieuthu041994@gmail.com>
2023-04-09 16:18:16 +0700

    Modal common - fixed bug load file module.scss

--- shoes-fe/src/components/Content/Modal/Modal.js
+++ shoes-fe/src/components/Content/Modal/Modal.js
@@ -1,18 +1,17 @@
-import './Modal.css'
-
+import styles from './Modal.module.scss'
 
 const Modal = ({ title, open, onCancel, children }) => {
     if (!open) { return null }
     return (
-        <div className="modal" >
-            <div className="modal__overlay"></div>
-            <div className="model__body">
-                <div className="header_modal">
-                    <label className="title_modal">{title}</label>
-                    <button className="button_close_modal"
+        <div className={styles.modal} >
+            <div className={styles.modal__overlay}></div>
+            <div className={styles.model__body}>
+                <div className={styles.header_modal}>
+                    <label className={styles.title_modal}>{title}</label>
+                    <button className={styles.button_close_modal}
                         onClick={onCancel}>X</button>
                 </div>
-                <div className="content_modal">
+                <div className={styles.content_modal}>
                     {children}
                 </div>
             </div>
