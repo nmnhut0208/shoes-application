d8e4326d5349a25ad80f5a79d47f629563d9d9d8
Kieu Thu <kieuthu041994@gmail.com>
2023-05-26 18:25:18 +0700

    create connection each execute

--- shoes-be/utils/sql_helper.py
+++ shoes-be/utils/sql_helper.py
@@ -1,52 +1,56 @@
 import pyodbc
 import pandas as pd
 
-conn = pyodbc.connect(driver='{ODBC Driver 17 for SQL Server}',
-                    #   server="MINH\SQLEXPRESS",
-                      server="DESKTOP-GT3LP7K\SQLEXPRESS",
-                      database="PT",
-                      trusted_connection="yes",
-                      mars_connection="yes"
-                      )
-
-cursor = conn.cursor()
 
 
+def execute_database(sql, action_type='read'):
+    conn = pyodbc.connect(driver='{ODBC Driver 17 for SQL Server}',
+                          #   server="MINH\SQLEXPRESS",
+                          server="DESKTOP-GT3LP7K\SQLEXPRESS",
+                          database="PT",
+                          trusted_connection="yes",
+                          mars_connection="yes")
+
+
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    if action_type == 'read':
+        data = pd.read_sql(sql, conn)
+        conn.close()
+        return data
+    else:
+        conn.commit()
+        conn.close()
+    
 def read_sql(tbn: str) -> pd.DataFrame:
     sql = f"SELECT * FROM {tbn}"
-    df = pd.read_sql(sql, conn)
-    return df
+    data = execute_database(sql)
+    return data
 
 
 def read_sql_custom(sql: str) -> pd.DataFrame:
-    df = pd.read_sql(sql, conn)
-    return df
+    data = execute_database(sql)
+    return data
+
 
 
 def execute_custom(sql: str) -> pd.DataFrame:
-    cursor.execute(sql)
-    conn.commit()
+    execute_database(sql, action_type='custom')
 
 
 def insert_sql(tbn: str, col: str, val: str) -> None:
     sql = f"INSERT INTO {tbn} ({col}) VALUES ({val})"
     print("sql: ", sql)
-    cursor.execute(sql)
-    conn.commit()
-
+    execute_database(sql, action_type='insert')
 
 def insert_many_records_sql(tbn: str, col: str, val: str) -> None:
     sql = f"INSERT INTO {tbn} ({col}) VALUES {val}"
-    cursor.execute(sql)
-    conn.commit()
+    execute_database(sql, action_type='insert')
 
 def update_sql(tbn: str, val: str, condition: str) -> None:
     sql = f"UPDATE {tbn} SET {val} WHERE {condition}"
-    cursor.execute(sql)
-    conn.commit()
-
+    execute_database(sql, action_type='update')
 
 def delete_sql(tbn: str, condition: str) -> None:
     sql = f"DELETE FROM {tbn} WHERE {condition}"
-    cursor.execute(sql)
-    conn.commit()
+    execute_database(sql, action_type='delete')
