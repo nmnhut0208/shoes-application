c6bc7a63b743c6c23359ed7acd622f5dca3798bb
kieuthu <kieuthu041994@gmail.com>
2023-10-15 11:53:28 +0700

    change fontsize

--- shoes-fe/src/components/Content/NghiepVu/DonHang/components/InputMau/InputMau.js
+++ shoes-fe/src/components/Content/NghiepVu/DonHang/components/InputMau/InputMau.js
@@ -7,7 +7,7 @@ const { Option } = Select;
 const customOptionStyle = {
   borderBottom: "1px solid #000", // Add a border line at the bottom of each option
   padding: "4px 0", // Adjust padding as needed
-  fontSize: "1.5rem",
+  fontSize: "1.7rem",
 };
 
 const filterOption = (input, option) => {
