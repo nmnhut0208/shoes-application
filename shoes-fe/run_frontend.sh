4f3aa851cdcbf2dc8f7eabc4fd3aac4868669200
kieuthu <kieuthu041994@gmail.com>
2024-02-24 15:21:04 +0700

    start app

--- shoes-fe/run_frontend.sh
+++ shoes-fe/run_frontend.sh
@@ -3,5 +3,5 @@
 # npm install
 # npm start & 
 #npx kill-port 3000 -y  #close old port
-ls > ls.log 2>&1 
-nohup uvicorn main:app > server.log 2>&1 &
+ls > ls.log 2>&1
+nohup serve -s build -l 3000 > server.log 2>&1 &
